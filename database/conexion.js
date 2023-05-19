719843893d3441d7c2c0250a6ab75def1a01e52e
Ruben Quispe <ruben.q002@gmail.com>
2023-05-19 00:08:48 -0400

    Version coneccion DB

--- database/conexion.js
+++ database/conexion.js
@@ -1,5 +1,8 @@
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_CONNECT);
+const sequelize = new Sequelize( process.env.SEQ_DB , process.env.SEQ_USER, process.env.SEQ_PASS, {
+    host: process.env.SEQ_HOST,
+    dialect: 'postgres'
+  });
 
 module.exports = {sequelize};
